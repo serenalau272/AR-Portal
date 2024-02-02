e5146df5c9f24f0c2de6fe512cb5dd580a9df14e
Serena Lau <serena.lau@sandfield.co.nz>
2024-02-02 17:08:01 +1300

    feat: copy over AR veranda project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontend Template
+Template for a website built in Vite + React + Typescript + Sass, following best practices.
+
+## Run Website
+Ensure all dependencies are installed
+```
+npm install
+```
+
+Run local server
+```
+npm run dev
+```
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './screens/Home'
+import './styles/globals.scss'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
